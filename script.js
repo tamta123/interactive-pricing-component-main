abb4e6236d9e898aceaef525f388d27c546b34c8
tamta123 <tamtag186@gmail.com>
2023-04-15 15:42:41 +0400

    added "view" change to slider event

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let views = document.querySelector("views");
+let views = document.querySelector(".views");
 let cost = document.querySelector (".price");
 let slider = document.getElementById ("vol");
 let toggle =  document.getElementById("billing");
 
-let pageViews = ['10k','50k','100k','500l','1M'];
+let pageViews = ['10k','50k','100k','500k','1M'];
 let perMonth = [8, 12, 16, 24, 36];
 
 let isYearly = false;
 
 slider.addEventListener('input',function(){
 updateValue();
+views.innerHTML = pageViews[slider.value]
 })
 
 toggle.addEventListener('change', function(){
@@ -17,12 +18,13 @@ if(isYearly == false){
     isYearly= true
 }else{ isYearly = false
     }
+    updateValue()
 })
 
 function updateValue(){
     if(isYearly){
-        cost.innerHTML=perMonth[slider.value]*.75
+        cost.innerHTML=(perMonth[slider.value]*.75).toFixed(2)
     } else{
-        cost.innerHTML=perMonth[slider.value]
+        cost.innerHTML=(perMonth[slider.value]).toFixed(2)
     }
 }
